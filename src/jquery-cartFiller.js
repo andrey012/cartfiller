687388fada6622a3a7b25030432b6efe3de6ccd5
andrey012 <andrey012@gmail.com>
2015-05-29 00:03:56 +0300

    refactoring bookmarklet generation

---
+++ src/jquery-cartFiller.js
@@ -0,0 +1,281 @@
+/**
+ * The jQuery core object
+ * @external jQuery
+ * @see {@link http://api.jquery.com/jQuery/}
+ */
+
+/**
+ * The jQuery plugin namespace.
+ * @external "jQuery.fn"
+ * @name "jQuery.fn"
+ * @memberof external jQuery
+ * @see {@link http://learn.jquery.com/plugins/|jQuery Plugins}
+ */
+
+/** @namespace CartFillerPlugin */
+
+;(function ( $, window, document, undefined ) {
+
+    'use strict';
+    /**
+     * @constant {string}
+     * @default
+     */
+    var pluginName = 'cartFillerPlugin';
+
+
+    /**
+     * @class cartFillerPlugin~Settings
+     */
+    var defaults = {
+        /**
+         * Choose Job URL
+         * @member {string} CartFillerPlugin~Settings#chooseJob
+         */
+        chooseJob: '',
+        /**
+         * Base URL to cartFiller files
+         * @member {string} CartFillerPlugin~Settings#baseUrl
+         */
+        baseUrl: '',
+        /**
+         * Injection method. Either 'script', 'eval' or 'iframe'
+         * @member {string} CartFillerPlugin~Settings#inject
+         * @default 'eval'
+         */
+        inject: 'script',
+        /**
+         * Whether to use minified version or not
+         * @member {boolean} CartFillerPlugin~Settings#minified
+         * @default true
+         */
+        minified: true,
+        /**
+         * Type of UI - either 'framed' or 'popup'
+         * @member {string} CartFillerPlugin~Settings#type
+         * @default 'framed'
+         */
+        type: 'framed',
+        /**
+         * Turn on debug features
+         * @member {boolean} CartFillerPlugin~Settings#debug
+         * @default false
+         */
+        debug: false,
+        /**
+         * Override URL of worker script
+         * @member {string} CartFillerPlugin~Settings#worker
+         * @default 'Ok'
+         */
+        worker: '',
+        /**
+         * Injects an alert for each step into the bookmarklet to troubleshoot
+         * @member {boolean} CartFillerPlugin~Settings#traceStartup
+         * @default false
+         */
+        traceStartup: false,
+        /**
+         * If set to true, then lengths of bookmarklets will be logged to console
+         * @member {boolean} CartFillerPlugin~Settings#logLength
+         * @default false
+         */
+        logLength: false
+    };
+
+    /**
+     * @class CartFillerPlugin~Plugin
+     * @access private
+     */
+    function Plugin ( element, options ) {
+        this.element = element;
+        this.settings = $.extend( {}, defaults, options );
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+    
+
+    $.extend(Plugin.prototype, {
+        /**
+         * Adds href attribute to an element
+         * @function ParseTablePlugin~Plugin#init
+         * @access private
+         */
+        init: function () {
+            var href;
+            if (this.settings.inject === 'script'){
+                href = this.scriptBookmarklet();
+            } else if (this.settings.inject === 'eval'){
+                href = this.evalBookmarklet();
+            } else if (this.settings.inject === 'iframe'){
+                href = this.iframeBookmarklet();
+            } else {
+                alert('invalid inject value, correct values are "script", "eval" and "iframe"');
+            }
+            if (this.settings.logLength) console.log('generated bookmarklet: ' + href.length);
+            $(this.element).attr('href', href);
+        },
+        getTypeId: function(){
+            if (this.settings.type === 'framed'){
+                return 0;
+            } else if (this.settings.type === 'popup'){
+                return 1;
+            } else {
+                alert('type not set or invalid, should be either "framed" or "popup"');
+                return 0;
+            }
+        },
+        getInjectUrl: function(){
+            var script;
+            if (this.settings.minified){
+                script = 'inject.min.js';
+            } else {
+                script = 'inject.js';
+            }
+            return '/boot/' + script;
+        },
+        getIframeUrl: function(){
+            return '/boot/i.htm';
+        },
+        trace: function(msg){
+            if (this.settings.traceStartup){
+                return "alert('" + msg + "');";
+            } else {
+                return "";
+            }
+        },
+        scriptBookmarklet: function(){
+            return "javascript:try{" +
+                this.trace("start") + 
+                "(function(d,c,a,t,o,u,v,j,k,x,y,s){" + 
+                    this.trace('in function') +
+                    "s=d.createElement('script');" + 
+                    this.trace('script element created') +
+                    "s[a](c+t,o);" +
+                    this.trace('type set') +
+                    "s[a](u,v+'?'+(new Date()).getTime());" + 
+                    this.trace('src set') +
+                    "s[a](c+j,k);" +
+                    this.trace('choose-job set') +
+                    "if(x)s[a](c+x,y);" + 
+                    this.trace('debug set') +
+                    "d.getElementsByTagName('head')[0].appendChild(s);" +
+                    this.trace('script element added') +
+                "})(" +
+                    "document," +
+                    "'data-'," +
+                    "'setAttribute'" +
+                    ",'type'," + this.getTypeId() + "," +
+                    "'src','" + this.settings.baseUrl + this.getInjectUrl() + "'," +
+                    "'choose-job','" + this.settings.chooseJob +"'," +
+                    "'debug'," + (this.settings.debug ? 1: 0) +
+                ");"+
+            "}catch(e){alert(e);}";
+        },
+        evalBookmarklet: function(){
+            return "javascript:try{" +
+                this.trace('start') +
+                "(function(x,t,u,v,j,d){" +
+                    this.trace('in function') +
+                    "x.open(" +
+                        "'GET'," +
+                        "u+v+'?'+(new Date()).getTime()," +
+                        "true" +
+                    ");" +
+                    this.trace('x opened') +
+                    "x.onload=function(){" +
+                        "try{" +
+                            this.trace('x onload called') +
+                            "eval(" +
+                                "'(function(){'+" +
+                                "x.response+" +
+                                "'}).call({cartFillerEval:[u,t,j,d]});'" +
+                            ");" +
+                            this.trace('eval complete') +
+                        "}catch(e){alert(e);}" +
+                    "};" +
+                    this.trace('x onload set') +
+                    "x.send();" +
+                    this.trace('x sent') +
+                "})(" +
+                    "new XMLHttpRequest()," + 
+                    this.getTypeId() + "," +
+                    "'" + this.settings.baseUrl + "'," +
+                    "'" + this.getInjectUrl() + "'," +
+                    "'" + this.settings.chooseJob + "'," +
+                    (this.settings.debug ? 1 : 0) +
+                ");" +
+            "}catch(e){alert(e);}";
+        },
+        iframeBookmarklet: function(type, minified, iframeDeltaUrl){
+            return "javascript:try{" +
+                this.trace('start') +
+                "(function(d,p,t,u,v,m,j,y,x,i){" +
+                    this.trace('in') +
+                    "window.addEventListener(" +
+                        "'message'," +
+                        "function(e){" +
+                            "try{" +
+                                this.trace('event') +
+                                "if(p.test(x=e.data)){" +
+                                    this.trace('event+') +
+                                    "eval(" +
+                                        "'(function(){'+" +
+                                        "x+" +
+                                        "'}).call({cartFillerEval:[u,t,j,y]});'" +
+                                   ");" +
+                                    this.trace('eval done') +
+                                "}" +
+                            "}catch(e){alert(e);}" +
+                        "}" +
+                        ",true" +
+                    ");" +
+                    this.trace('lstnr+') +
+                    "i=d.createElement('iframe');" +
+                    this.trace('iframe') +
+                    "d.getElementsByTagName('body')[0].appendChild(i);" +
+                    this.trace('iframe+') +
+                    "i.contentWindow.location.href=u+v+(m?'?min&':'?')+(new Date()).getTime();" +
+                    this.trace('iframe++') +
+                "})(" +
+                    "document," +
+                    "/^\'cartFillerEval\'/," + 
+                    this.getTypeId() + "," +
+                    "'" + this.settings.baseUrl +"'," +
+                    "'" + this.getIframeUrl() + "'," + 
+                    (this.settings.minified ? 1:0) + "," +
+                    "'" + this.settings.chooseJob + "'," +
+                    (this.settings.debug ? 1 : 0) +
+                ");" +
+            "}catch(e){alert(e);}";
+        }
+    });
+
+    /**
+     * Plugin function. Should be used on link elemens
+     * <a id="bookmarklet">
+     *     Bookmarklet - drag this link to your bookmarks
+     * </a>
+     * <script>
+     *     $("#bookmarklet").cartFillerPlugin({
+     *         minified: true,
+     *         inject: 'script',
+     *         type: 'framed'
+     *     });
+     * </script>
+     * @function external:"jQuery.fn".cartFillerPlugin
+     * @global
+     * @name "jQuery.fn.cartFillerPlugin"
+     * @param {CartFillerPlugin~Settings} options
+     * @returns {external:jQuery}
+     * @access public
+     */
+    $.fn[ pluginName ] = function ( options ) {
+            return this.each(function() {
+                    if ( !$.data( this, 'plugin_' + pluginName ) ) {
+                            $.data( this, 'plugin_' + pluginName, new Plugin( this, options ) );
+                    }
+            });
+    };
+
+})( jQuery, window, document );
