503b5466f08bc17cc11b340eb8e228c14c2999c6
andrey012 <andrey012@gmail.com>
2015-09-02 15:28:53 +0300

    Workaround for gh-pages

--- testsuite/worker.js
+++ testsuite/worker.js
@@ -5,6 +5,10 @@
 (function(window, document, undefined){
     cartFillerAPI().registerWorker(function(window, document, api, task, job){
         var baseUrl = job.rootCartfillerPath.replace(/\/[^\/]+\/?$/, '') + '/samples/sample-shop.html';
+        var githubPattern = /^https?\:\/\/andrey012.github.io\/cartfiller\//;
+        if (githubPattern.test(job.rootCartfillerPath)) {
+            baseUrl = githubPattern.exec(job.rootCartfillerPath)[0];
+        }
         return {
             sayHello: [
                 'open homepage', function() {
