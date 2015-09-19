f38e9e1b6067ff781b180a1bb77a87e85d2591be
andrey012 <andrey012@gmail.com>
2015-09-19 15:29:11 +0300

    cosmetics

--- dist/inject.js
+++ dist/inject.js
@@ -184,7 +184,7 @@
      * @member {String} CartFiller.Configuration#gruntBuildTimeStamp
      * @access public
      */
-    config.gruntBuildTimeStamp='1468154513977';
+    config.gruntBuildTimeStamp='1468181845319';
 
     // if we are not launched through eval(), then we should fetch
     // parameters from data-* attributes of <script> tag
