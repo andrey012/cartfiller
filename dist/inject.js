f1f17d855218d4ba9a88e98219729471cd195ee0
andrey012 <andrey012@gmail.com>
2015-09-28 15:45:04 +0300

    limit number of messages shown in local.html

--- dist/inject.js
+++ dist/inject.js
@@ -184,7 +184,7 @@
      * @member {String} CartFiller.Configuration#gruntBuildTimeStamp
      * @access public
      */
-    config.gruntBuildTimeStamp='1472109733271';
+    config.gruntBuildTimeStamp='1473062870669';
 
     // if we are not launched through eval(), then we should fetch
     // parameters from data-* attributes of <script> tag
